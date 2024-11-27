164849fc5dadc2a659c42f9b22b63443eb864556
MyangSZ <unoyou@naver.com>
2024-11-27 20:44:07 +0900

    1st_session-counter

---
+++ 하미향/1st_session-counter/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="content">
+        <div className="count">
+          <h1>count : {count}</h1>
+        </div>
+        <div className="button">
+          <button
+            className="btn"
+            onClick={() => {
+              setCount((prev) => prev + 1);
+            }}
+          >
+            +1
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setCount((prev) => prev - 1);
+            }}
+          >
+            -1
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Counter;
